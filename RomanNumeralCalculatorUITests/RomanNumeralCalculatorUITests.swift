fddd6c0b5ed9c15e3c65aea95e2dd2c34051b697
Abdul Wahib <wahib1993@gmail.com>
2017-02-22 21:49:50 +0500

    add toRoman unit test

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -2,8 +2,8 @@
 //  RomanNumeralCalculatorUITests.swift
 //  RomanNumeralCalculatorUITests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Abdul Wahib on 2/22/17.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
